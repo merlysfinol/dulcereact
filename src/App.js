68c7bc75ff6c3172d71448e97e20dd48f73477d0
Merlys Finol <merlysfinol@gmail.com>
2021-06-17 11:35:06 -0400

    Comienzo...

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './img/logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Dulce Fusión en React
+        </p>
+        <p>
+          Muy pronto....
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
